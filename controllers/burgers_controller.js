014f710b253a31b44222ed140f91903519dc6677
Bhoward19 <bhoward19@ymail.com>
2018-10-11 20:53:30 -0400

    attempt

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,4 @@
-/*
-Below are the routes which will be needed by the app
-*/
+
 var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 var PORT = process.env.PORT || 3000; 
 
 
+
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({
@@ -18,7 +19,15 @@ app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+//ROUTER
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
