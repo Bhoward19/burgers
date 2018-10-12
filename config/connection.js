2696c472f2dac7d33519b9b1e27b57791d6c55d1
Bhoward19 <bhoward19@ymail.com>
2018-10-11 20:38:57 -0400

    tired

--- config/connection.js
+++ config/connection.js
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_AQUA_URL)  {
-	connection = mysql.createConnection(process.env.JAWSDB_AQUA_URL);
+if (process.env.JAWSDB_URL)  {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
 		host: "localhost",
